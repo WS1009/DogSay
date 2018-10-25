03b07306b631dc07833ddb739df054de871073f3
WS1009 <WANGSHUN611@163.com>
2018-10-25 22:26:00 +0800

    增加视频详情页的视频播放功能

--- app/Detail.js
+++ app/Detail.js
@@ -11,6 +11,7 @@ import {
 import Icon from 'react-native-vector-icons/Ionicons'
 const {width, height}= Dimensions.get('window');
 import request from './request'
+import Video from 'react-native-video'
 
 //npm i react-native-video --save
 //rnpm link react-native-video
@@ -24,13 +25,10 @@ export default class Detail extends Component {
             rate:1,
             mute:false,
             repeat:false,
-            resizeMethod:'contain',
+            resizeMode:'contain',
         })
     }
 
-    _up(){
-
-    }
 
     _pressRow(){
         alert('Hello')
@@ -45,13 +43,54 @@ export default class Detail extends Component {
         }
     }
 
+    _onLoadStart(){
+        console.log('_onLoadStart')
+    }
+
+    _onLoad(){
+        console.log('_onLoad')
+    }
+
+    _onProgress(){
+        console.log('_onProgress')
+    }
+
+    _onEnd(){
+        console.log('_onEnd')
+    }
+
+    _onError(){
+        console.log('_onError')
+    }
+
+
     render() {
         var data=this.state.data;
         return (
             <View style={styles.page}>
                 <Text onPress={this._backToList} style={{fontSize:18,padding: 15,color:'red'}}>详情页面</Text>
                 <View style={styles.videoBox}>
-                    //视频部分
+                    <Video
+                        ref='videoPlayer'
+                        source={{uri:data.video}}
+                        style={styles.video}
+                        //声音的放大倍数
+                        volume={3}
+                        //是否停止
+                        paused={false}
+                        //是否暂停
+                        rate={this.state.rate}
+                        //是否静音
+                        muted={this.state.mute}
+                        //显示方式
+                        resizeMode={this.state.resizeMode }
+                        repeat={this.state.repeat}
+                        onLoadStart={this._onLoadStart}
+                        onLoad={this._onLoad}
+                        onProgress={this._onProgress}
+                        onEnd={this._onEnd}
+                        onError={this._onError}
+                    />
                 </View>
             </View>
         );

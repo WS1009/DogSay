d68026f68a5db56f681969783e1cebe373fc9614
WS1009 <WANGSHUN611@163.com>
2018-10-29 22:25:31 +0800

    增加播放进度条（一）

--- app/Detail.js
+++ app/Detail.js
@@ -9,7 +9,8 @@ import {
     Dimensions,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
-const {width, height}= Dimensions.get('window');
+
+const {width, height} = Dimensions.get('window');
 import request from './request'
 import Video from 'react-native-video'
 
@@ -19,60 +20,96 @@ import Video from 'react-native-video'
 export default class Detail extends Component {
     constructor(props) {
         super(props);
-        var data=this.props.row;
-        this.state=({
-            data:data,
-            rate:1,
-            mute:false,
-            repeat:false,
-            resizeMode:'contain',
+        var data = this.props.row;
+        this.state = ({
+            data: data,
+            rate: 1,
+            mute: false,
+            repeat: false,
+            resizeMode: 'contain',
+            videoLoaded: false,
+            playing: false,
+            videoProgress: 0.01,
+            videoTotal: 0,
+            currentTime: 0,
         })
     }
 
 
-    _pressRow(){
+    _pressRow() {
         alert('Hello')
     }
 
-    _backToList(){
-        let{navigator}=this.props;
+    _backToList() {
+        let {navigator} = this.props;
         if (navigator) {
             this.props.navigator.pop()
-        }else{
+        } else {
             alert(data._id)
         }
     }
 
-    _onLoadStart(){
+    _onLoadStart() {
         console.log('_onLoadStart')
     }
 
-    _onLoad(){
+    _onLoad() {
         console.log('_onLoad')
     }
 
-    _onProgress(){
+    _onProgress(data) {
+        if (!this.state.videoLoaded) {
+            this.setState({
+                videoLoaded: true,
+            })
+        }
+        var duration = data.playableDuration;
+        var currentTime = data.currentTime;
+        var percent = Number((currentTime / duration).toFixed(2));
+        var newState={
+            videoTotal: duration,
+            currentTime: Number(data.currentTime.toFixed(2)),
+            videoProgress: percent,
+        };
+
+        if (!this.state.videoLoaded) {
+            newState.videoLoaded=true;
+        }
+
+        if (!this.state.videoLoaded) {
+            newState.playing=true;
+        }
+
+        this.setState({
+            videoTotal: duration,
+            currentTime: Number(data.currentTime.toFixed(2)),
+            videoProgress: percent,
+            playing:true,
+        });
         console.log('_onProgress')
     }
 
-    _onEnd(){
+    _onEnd() {
+        this.setState({
+            videoProgress: 1,
+        });
         console.log('_onEnd')
     }
 
-    _onError(){
+    _onError() {
         console.log('_onError')
     }
 
 
     render() {
-        var data=this.state.data;
+        var data = this.state.data;
         return (
             <View style={styles.page}>
-                <Text onPress={this._backToList} style={{fontSize:18,padding: 15,color:'red'}}>详情页面</Text>
+                <Text onPress={this._backToList} style={{fontSize: 18, padding: 15, color: 'red'}}>详情页面</Text>
                 <View style={styles.videoBox}>
                     <Video
                         ref='videoPlayer'
-                        source={{uri:data.video}}
+                        source={{uri: data.video}}
                         style={styles.video}
                         //声音的放大倍数
                         volume={3}
@@ -83,14 +120,28 @@ export default class Detail extends Component {
                         //是否静音
                         muted={this.state.mute}
                         //显示方式
-                        resizeMode={this.state.resizeMode }
+                        resizeMode={this.state.resizeMode}
                         repeat={this.state.repeat}
                         onLoadStart={this._onLoadStart}
                         onLoad={this._onLoad}
                         onProgress={this._onProgress}
                         onEnd={this._onEnd}
-                        onError={this._onError}
-                    />
+                        onError={this._onError}/>
+                    {
+                        !this.state.videoLoaded && <ActivityIndicatorIOS color='#ee735c' style={styles.loading}/>
+                    }
+
+                    {
+                        this.state.videoLoaded && !this.state.playing ?
+                        <Icon onPress={this._replay} name='play' style={styles.playIcon}></Icon>:null
+                    }
+
+                    <View style={styles.progressBox}>
+                        <View style={[styles.progressBar, {width: width * this.state.videoProgress}]}>
+
+                        </View>
+                    </View>
+
                 </View>
             </View>
         );
@@ -99,20 +150,36 @@ export default class Detail extends Component {
 
 
 const styles = StyleSheet.create({
-    page:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#ffffff',
+    page: {
+        flex: 1,
+        backgroundColor: '#ffffff',
     },
-    videoBox:{
-        width:width,
+    videoBox: {
+        width: width,
         height: 360,
-        backgroundColor:'#000',
+        backgroundColor: '#000',
     },
-    video:{
-        width:width,
-        height:360,
-        backgroundColor:'#000',
+    video: {
+        width: width,
+        height: 360,
+        backgroundColor: '#000',
+    },
+    loading: {
+        position: 'absolute',
+        left: 0,
+        top: 140,
+        width: width,
+        alignSelf: 'center',
+        backgroundColor: 'transparent',
+    },
+    progressBox: {
+        width: width,
+        height: 2,
+        backgroundColor: '#ccc',
+    },
+    progressBar: {
+        width: 1,
+        height: 2,
+        backgroundColor: '#ff6600',
     },
 });
